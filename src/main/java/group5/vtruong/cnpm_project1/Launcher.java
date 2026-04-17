2e4027913801aa7557acd06b95da3c63aeb59a46
Truong1866 <truongvu06678@gmail.com>
2026-04-18 00:27:37 +0700

    update main

--- src/main/java/group5/vtruong/cnpm_project1/Launcher.java
+++
@@ -1,9 +0,0 @@
-package group5.vtruong.cnpm_project1;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
