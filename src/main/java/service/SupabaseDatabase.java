e32b968550a0be2dc6b138159b175f6215ea992c
Truong1866 <truongvu06678@gmail.com>
2026-05-07 20:31:44 +0700

    add Dotenv for secu code

--- src/main/java/service/SupabaseDatabase.java
+++ src/main/java/service/SupabaseDatabase.java
@@ -2,15 +2,17 @@ package service;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class SupabaseDatabase {
+    private static final Dotenv dotenv = Dotenv.load();
     public static Connection connect(String url, String user, String password) throws Exception{
         Connection conn = DriverManager.getConnection(url, user, password);
         return conn;
     }
 
     public static Connection connect() throws Exception{
-        return connect(doten.get(DB_URL), doten.get(DB_USER), doten.get(DB_PASSWORD);
+        return connect(dotenv.get(DB_URL), dotenv.get(DB_USER), dotenv.get(DB_PASSWORD));
     }
 }
 
