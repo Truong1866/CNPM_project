083155b41871a6126a207aad59ca363e6225bbe5
Truong1866 <truongvu06678@gmail.com>
2026-05-08 13:24:20 +0700

    update

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -17,8 +17,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        //code dùng cho chạy test thử 1 file fxml nào đó
         SessionUser user = new SessionUser("00001", "000001", "ADMIN");
         AuthManager.login(user);
+        //Test thử file fxml thì thay đổi về đúng đường dẫn
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/ResidentView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setScene(scene);
